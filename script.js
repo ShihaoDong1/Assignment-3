231ed33a33be49835bbb0d006b3a735583677556
Michael Odusanya <modusanya12@gmail.com>
2020-06-01 18:38:57 -0400

    js file works with html now lol

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log(contain); //Question 2
 console.log(document.querySelector("section")); // Question 3
 console.log(document.querySelectorAll(".second")); //Question 4
 console.log(document.getElementsByTagName("ol")[0].children[2]); //Question 5
-contain.textContent = "Hello"; //Question 6
+//contain.textContent = "Hello"; //Question 6
 document.getElementsByTagName("div")[1].classList.add("main") 
 document.getElementsByTagName("div")[1].classList.remove("main") //Question 7
 let lee = document.createElement("li");//Question 8
